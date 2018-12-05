9ca220eb8abad71c9b2ad0171c4910dc93bd66f3
Harry Pidcock <haza55@gmail.com>
2018-12-05 11:15:48 +1000

    Improve unpack for casting

--- unpack.go
+++ unpack.go
@@ -6,12 +6,17 @@ import (
 	"github.com/golang/protobuf/ptypes/any"
 )
 
-// Unpack returns a unpacked or an error
-func Unpack(a *any.Any) (proto.Message, error) {
+// Unpack returns a unpacked or an error (will panic if eout is nil on error)
+func Unpack(a *any.Any, eout *error) proto.Message {
 	var da ptypes.DynamicAny
 	err := ptypes.UnmarshalAny(a, &da)
 	if err != nil {
-		return nil, err
+		if eout != nil {
+			*eout = err
+		} else {
+			panic(err)
+		}
+		return nil
 	}
-	return da.Message, nil
+	return da.Message
 }
